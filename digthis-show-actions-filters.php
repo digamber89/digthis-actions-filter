323ec1517cc31870775d3625ec6f70cf91eef994
digamber pradhan <digamberpradhan89@gmail.com>
2016-02-27 22:59:41 +0545

    Initially Upload Files to GitHub

---
+++ digthis-show-actions-filters.php
@@ -0,0 +1,72 @@
+<?php
+/*
+Plugin Name: Digthis Show Action Hook
+Description: Simple Plugin To See filter and action hooks
+Plugin URI: http://#
+Author: 
+Author URI: http://www.digamberpradhan.com.np/
+Version: 1.0
+License: GPL2
+Text Domain: digthis-show-filters
+*/
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+if ( !defined( 'DIGTHIS_PLUGIN_DIR_PATH') ) {
+	define('DIGTHIS_PLUGIN_DIR_PATH', dirname(__FILE__) );
+}
+if( !defined( 'DIGTHIS_PLUGIN_FILE_PATH') ){
+	define('DIGTHIS_PLUGIN_FILE_PATH', __FILE__ );	
+}
+
+
+class digthisShowFilters{
+	/* instance of class*/
+	public static $instance;
+	public $menu_page;
+	/* function to get instance of class */
+	public static function get_instance(){
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	public function __construct(){
+		add_action( 'admin_menu', array($this, 'digthis_admin_menu' ) );
+		add_action( 'admin_enqueue_scripts', array($this, 'digthis_enqueue_scripts') );
+	}
+
+	public function digthis_admin_menu() {
+		$this->menu_page = add_menu_page( 
+			'Digthis Show Filters',
+		 	'View Filters',
+		 	'manage_options',
+		 	'digthis-show-filters',
+		 	array($this, 'generate_admin_page')
+		 );
+	}
+
+	public function generate_admin_page(){
+		require_once(DIGTHIS_PLUGIN_DIR_PATH.'/view/admin.php');
+	}
+
+	public function digthis_enqueue_scripts() {
+		$screen = get_current_screen();
+		wp_register_style( 
+				'select2',
+				plugins_url( 'assets/css/select2.min.css', DIGTHIS_PLUGIN_FILE_PATH ),
+				false, false, 'ALL' );
+			
+		wp_register_script(
+				'select2',
+				plugins_url( 'assets/js/select2.min.js', DIGTHIS_PLUGIN_FILE_PATH ),
+				array('jquery'),
+				false,
+				true );
+		if( $this->menu_page === $screen->id ) {
+			wp_enqueue_style(  'select2' );
+			wp_enqueue_script( 'select2' );
+		}
+	}
+
+}
+add_action('plugins_loaded', array('digthisShowFilters','get_instance'), 10);
---
+++ view/admin.php
@@ -0,0 +1,69 @@
+<div class="wrap">
+	<h1>See Hooked Actions and Filters</h1>
+	<form method="post" action="">
+		<table class="form-table">
+			<tr>
+				<th>Select Filter</th>
+				<td>
+					<select id="wordpress-filters" name="wordpress-filters">
+					<?php
+						global $wp_filter;
+						foreach( $wp_filter as $filter_key => $hooked_values ){
+					?>
+						<option value="<?php echo $filter_key; ?>"><?php echo $filter_key; ?></option>
+					<?php
+					}
+					?>
+					</select>
+				</td>
+				<td>
+				<input type="submit" class="button button-primary">
+				</td>
+			</tr>
+		 </table>	
+	</form>
+	<div id="result">
+	<?php
+	  
+	 if( filter_input(INPUT_POST, 'wordpress-filters') ):
+	 	$function_name = filter_input(INPUT_POST, 'wordpress-filters');
+	?>
+		<table class="wp-list-table widefat striped">
+		<tr>
+			<th>SN#</td>
+			<th>Function Name</th>
+			<th>Priority</th>
+			<th>Function Details</th>
+			<th>Arguments Accepted</th>
+		</tr>
+		<?php
+			$count = 1;
+			$hooked_functions = $wp_filter['init'];
+			foreach($hooked_functions as $priority => $hooked_function){
+				foreach ($hooked_function as $function_name => $function_variables) {
+				?>
+				<tr>
+					<td><?php echo $count; ?></td>
+					<td><?php echo $function_name; ?></td>
+					<td><?php echo $priority; ?></td>
+					<td><?php echo $function_variables['accepted_args']; ?></td>
+					<td><input type="button" class="button" value="See Details"></td>
+				</tr>
+				<?php
+				$count++;
+				}
+				
+			}
+		?>
+		</table>
+	<?php endif; ?>
+	</div>
+</div>
+<script type="text/javascript">
+	jQuery(function($){
+		$('#wordpress-filters').select2();
+		$('#wordpress-filters').on('change', function(){
+			console.log( $(this).val() );
+		});
+	})
+</script>
